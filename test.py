642ba356206fa3a36d4b863ca9a70f9693c5cb20
Mehedi <mehedi.interface@gmail.com>
2025-06-22 22:01:30 +0300

    arithmetic

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+"""
+Simple Arithmetic:
+Ask the user for two numbers,
+then print their sum,
+difference,
+product,
+and quotient.
+"""
+num_1 = float(input("Please enter your 1st number: "))
+num_2 = float(input("Please enter your 2nd number: "))
+
+print(f"Sum: {num_1 + num_2}")
+print(f"Difference: {num_1 - num_2}")
+print(f"Product: {num_1 * num_2}")
+print(f"Quotient: {num_1 / num_2}")
+
