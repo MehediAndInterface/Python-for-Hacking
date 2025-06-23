5f4eb5cd8a9c8395bdf74da1d8a27be0e93a915f
Mehedi <mehedi.interface@gmail.com>
2025-06-23 21:36:58 +0300

    6 - data types

--- Part_1_50_Foundational_Python_Problems/Basics/5_String_Concatenation.py
+++ Part_1_50_Foundational_Python_Problems/Basics/5_String_Concatenation.py
@@ -3,5 +3,4 @@ String Concatenation: Create two string variables and combine them to form a sin
 """
 var_1 = "Hello,"
 var_2 = "World!"
-
 print(f"{var_1} {var_2}")
---
+++ Part_1_50_Foundational_Python_Problems/Basics/6_Data_Types.py
@@ -0,0 +1,11 @@
+"""
+Data Types: Create variables of type
+integer, float, string, and boolean,
+and print the type of each variable using the type() function.
+"""
+
+var_int = 10
+var_float = 10.5
+var_string = "Hello"
+var_boolean = True
+print(type(var_boolean))
