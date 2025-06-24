3fd94a44aef4697fa19ce29353e5f13c9b654c6c
Mehedi <mehedi.interface@gmail.com>
2025-06-24 20:30:23 +0300

    7 - area of rect.

---
+++ Part_1_50_Foundational_Python_Problems/Basics/7_Area_of_a_Rectangle.py
@@ -0,0 +1,12 @@
+"""
+Area of a Rectangle: Ask for the length and width of a rectangle and calculate its area.
+
+area of rectangle = length × width
+"""
+length = float(input("Please enter length of rectangle: "))
+width = float(input("Please enter width of rectangle: "))
+
+area = length * width
+
+print(f"Area of rectangle is: {area:.2f}")
+
