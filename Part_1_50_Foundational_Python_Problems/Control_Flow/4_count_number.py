ae3e2e9b4379bb04d0f2573a3ad3c9098b18e855
Mehedi <mehedi.interface@gmail.com>
2025-06-29 20:45:05 +0300

    count number

---
+++ Part_1_50_Foundational_Python_Problems/Control_Flow/4_count_number.py
@@ -0,0 +1,19 @@
+"""
+Count from 1 to 10: Use a for loop to print the numbers 1 through 10.
+
+For Loop:
+    - for loop is a programming construct that allows you to execute a block of code a specific number of times.
+    - Think of it as a way to automate a repetitive task. Instead of writing the same code over and over again
+    - The two main scenarios where a for loop is used are:
+            - Iterating over a sequence: This is when you have a collection of items (like a list of numbers, a string of characters, or lines in a file) and you want to do something with each item one by one.
+            - Repeating an action a known number of times: If you know you need to perform a task exactly 10 times, a for loop is the perfect tool for the job.
+
+for number in range(5):
+    print("This is iteration number:", number)
+"""
+
+for number in range(1, 11):
+    print(number)
+
+
+
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 """
 Simple Login: Create variables for a username and password. Ask the user for theirs and check if they match.
 """
+username = "admin"
+password = "password123"
+
+user_input_username = input("Please enter your username: ")
+user_input_password = input("Please enter your password: ")
+
+if user_input_username == username and user_input_password == password:
+    print(f"\nLogin successful! Welcome, {username}")
+else:
+    print("\nError: Incorrect username or password.")
