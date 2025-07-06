31887ba945848cf68cd4c18c35963ea876504b68
Mehedi <mehedi.interface@gmail.com>
2025-07-06 21:33:02 +0300

    access list

---
+++ Part_1_50_Foundational_Python_Problems/Data_Structures/1_Create_a_List.py
@@ -0,0 +1,6 @@
+"""
+Create a List: Create a list of your favorite fruits and print the entire list.
+"""
+
+fruits = ["Mango", "Banana", "Orange", "Apple"]
+print(fruits)
---
+++ Part_1_50_Foundational_Python_Problems/Data_Structures/3_Modify_a_List.py
@@ -0,0 +1,7 @@
+"""
+Modify a List: Change the second item in the list to a different fruit.
+"""
+fruits = ["apple", "banana", "cherry", "mango", "papaya"]
+print(fruits)
+fruits[1] = "strawberry"
+print(fruits)
