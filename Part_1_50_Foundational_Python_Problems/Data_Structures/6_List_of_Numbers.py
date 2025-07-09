9785598dad16d469d923ba209111d28ec380946e
Mehedi <mehedi.interface@gmail.com>
2025-07-09 21:10:31 +0300

    list of numbers

---
+++ Part_1_50_Foundational_Python_Problems/Data_Structures/6_List_of_Numbers.py
@@ -0,0 +1,11 @@
+"""
+List of Numbers: Create a list of 5 numbers and find their sum and average.
+"""
+numbers = [1, 2, 3, 4, 5]
+total = sum(numbers)
+
+print(total)
+
+avg = total / len(numbers)
+print(avg)
+
